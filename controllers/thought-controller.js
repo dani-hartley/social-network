8ecd1422a734eb063a9a1832e792a9b9743c8ae7
dhartley <dani_hartley@msn.com>
2021-07-23 16:49:03 -0500

    added add reaction and delete reaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -50,6 +50,35 @@ const thoughtController = {
         })
         .catch(err => {res.status(400).json(err)});
     },
+    //add reaction
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId},
+            { $push: { reactions: body} },
+            { new: true, runValidators: true }
+        )
+        .then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    },
+    //remove reaction
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res.json(err));
+    },
     // delete thought by id
     deleteThought({ params }, res) {
         Thought.findOneAndDelete({ _id: params.thoughtId })
