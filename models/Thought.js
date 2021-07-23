fbac82347a3954b8f1cee6aaa54150bd08c2d198
dhartley <dani_hartley@msn.com>
2021-07-23 11:07:31 -0500

    added reaction model, updated user model

--- models/Thought.js
+++ models/Thought.js
@@ -1,11 +1,41 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: [280, 'Text can only be 280 characters']
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal = dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+)
+
+
 const ThoughtSchema = new Schema( 
     {
         thoughtText: {
             type: String,
-            require: true,
+            required: true,
             min: [1, 'Please enter text'],
             max: [280, 'Text can only be 280 characters']
         },
@@ -16,7 +46,7 @@ const ThoughtSchema = new Schema(
         },
         username: {
             type: String,
-            require: true
+            required: true
         },
         reactions: [ReactionSchema]
     },
--- models/User.js
+++ models/User.js
@@ -22,7 +22,12 @@ const UserSchema = new Schema(
                 ref: 'Thought'
             }
         ],
-        friends: []
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
     },
     {
         toJSON: {
